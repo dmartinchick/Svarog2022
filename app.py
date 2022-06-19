beb906122d067c87863e5964af8655a1c0f9338a
dmartinchick <dmartinchick@gmail.com>
2022-06-19 14:29:48 +0300

    try fix to sleep

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 """start app"""
+import asyncio
 from aiogram import executor
 
 from loader import dp
 import middlewares  # pylint: disable=unused-import
 import filters      # pylint: disable=unused-import
 import handlers     # pylint: disable=unused-import
+from utils.db_api.db_comands import set_loging_update
 
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
@@ -19,6 +21,13 @@ async def on_startup(dispatcher):
     # Уведомляет про запуск
     await on_startup_notify(dispatcher)
 
+async def schedule_check(wait_for):
+    """функция отправки запросов к ДБ"""
+    while True:
+        await asyncio.sleep(wait_for)
+
+        set_loging_update()
 
 if __name__ == '__main__':
+    dp.loop.create_task(schedule_check(30))
     executor.start_polling(dp, on_startup=on_startup)
